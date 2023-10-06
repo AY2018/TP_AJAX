1c614e055ab9583f25ad482abcb37711a7a71bd3
Ayoub Kahfy <ayoub.kahfy@gmail.com>
2023-10-06 10:07:36 +0200

    finalIthink

--- app.js
+++ app.js
@@ -1,87 +1,105 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let currentFilters = [];
+// Liste initiale de filtres vides
+let currentFilters = [];
 
-    // Récupération des données des plats
-    fetch('data.json')
-        .then(response => response.json())
-        .then(data => displayPlats(data.plats));
+// Liste pour stocker tous les plats récupérés du fichier JSON
+let allPlats = [];
 
-    function displayPlats(plats) {
-        const ul = document.querySelector('main ul');
-        ul.innerHTML = '';
-        
-        const filteredPlats = plats.filter(plat => 
-            currentFilters.every(filter => plat.classes.includes(filter))
-        );
-
-        filteredPlats.forEach(plat => {
-            ul.innerHTML += `
-                <li>
-                    <figure>
-                        <img src="${plat.imgSrc}" alt="">
-                        <div class="difficulty">
-                            ${getStars(plat.classes.find(cls => ["Facile", "Modéré", "Difficile"].includes(cls)))}
-                        </div>
-                        <div class="time">
-                            <i class="fa-solid fa-clock"></i>
-                            <p>${getTime(plat.classes.find(cls => ["Rapide", "Moyen", "Long"].includes(cls)))}</p>
-                        </div>
-                    </figure>
-                    <section class="img_content">
-                        <h1>${plat.titre}</h1>
-                        <div class="components">
-                            ${getComponents(plat.classes)}
-                        </div>
-                    </section>
-                </li>
-            `;
-        });
-    }
-
-    function getStars(difficulty) {
-        const starsCount = { "Facile": 1, "Modéré": 2, "Difficile": 3 }[difficulty];
-        return '<i class="fa-solid fa-star"></i>'.repeat(starsCount);
-    }
-
-    function getTime(time) {
-    const times = {
-        "Rapide": "-10 min",
-        "Moyen": "10 - 20 min",
-        "Long": "+20 min"
+function loadPlats() {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'data.json', true);
+    xhr.onload = function() {
+        if (this.status === 200) {
+            const data = JSON.parse(this.responseText);
+            allPlats = data.plats;
+            displayPlats(allPlats);
+        }
     };
-    return times[time];
+    xhr.send();
 }
 
-
-    function getComponents(classes) {
-        const icons = {
-            "Tomate": "./img/005-tomato.png",
-            "Fromage": "./img/004-cheese.png",
-            "Béchamel": "./img/003-milk-bottle.png",
-            "Boeuf": "./img/002-meat.png",
-            "Poulet": "./img/001-chicken-leg.png",
-            "Sans viande": "./img/001-vegetable.png"
-        };
-
-        return classes.filter(cls => Object.keys(icons).includes(cls))
-            .map(cls => `<div class="component"><img src="${icons[cls]}" alt=""><p>${cls}</p></div>`)
-            .join('');
-    }
-
-    document.querySelectorAll("article.boutons button").forEach(button => {
-        button.addEventListener('click', function() {
-            const filter = this.textContent.trim();
-            if (currentFilters.includes(filter)) {
-                currentFilters = currentFilters.filter(f => f !== filter);
-                this.classList.remove('active');
-            } else {
-                currentFilters.push(filter);
-                this.classList.add('active');
-            }
-
-            fetch('data.json')
-                .then(response => response.json())
-                .then(data => displayPlats(data.plats));
-        });
+// Bouton functions 
+document.querySelectorAll("article.boutons button").forEach(button => {
+    button.addEventListener('click', function() {
+        const filter = this.textContent.trim();
+        if (currentFilters.includes(filter)) {
+            currentFilters = currentFilters.filter(f => f !== filter);
+            this.classList.remove('active');
+        } else {
+            currentFilters.push(filter);
+            this.classList.add('active');
+        }
+        const filteredPlats = allPlats.filter(plat => 
+            currentFilters.every(filter => plat.classes.includes(filter))
+        );
+        displayPlats(filteredPlats);
     });
 });
+
+// Créer les plats/cartes
+
+function displayPlats(plats) {
+    const ul = document.querySelector('main ul');
+    ul.innerHTML = '';  // Supprime les plats précédemment affichés
+
+    plats.forEach(plat => {
+        ul.innerHTML += `
+            <li>
+                <figure>
+                    <img src="${plat.imgSrc}" alt="" loading="lazy">
+                    <div class="difficulty">
+                        ${getStars(plat.difficulté)}
+                    </div>
+                    <div class="time">
+                        <i class="fa-solid fa-clock"></i>
+                        <p>${getTime(plat.temps)}</p>
+                    </div>
+                </figure>
+                <section class="img_content">
+                    <h1>${plat.titre}</h1>
+                    <div class="components">
+                        ${getComponents(plat.classes)}
+                    </div>
+                </section>
+            </li>
+        `;
+    });
+}
+
+function getStars(difficulté) {
+    switch(difficulté) {
+        case "Facile": return '<i class="fa-solid fa-star"></i>';
+        case "Modéré": return '<i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i>';
+        case "Difficile": return '<i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i>';
+        default: return '';
+    }
+}
+
+function getTime(temps) {
+    switch(temps) {
+        case "Rapide": return '10 min';
+        case "Moyen": return '20 min';
+        case "Long": return '45 min';
+        default: return '';
+    }
+}
+
+function getComponents(classes) {
+    const relevantClasses = ["Tomate", "Fromage", "Béchamel", "Boeuf", "Poulet", "Sans viande"];
+
+    const classToIcon = {
+        "Tomate": '<img src="./img/005-tomato.png" alt="Tomate">',
+        "Fromage": '<img src="./img/004-cheese.png" alt="Fromage">',
+        "Béchamel": '<img src="./img/003-milk-bottle.png" alt="Béchamel">',
+        "Boeuf": '<img src="./img/002-meat.png" alt="Boeuf">',
+        "Poulet": '<img src="./img/001-chicken-leg.png" alt="Poulet">',
+        "Sans viande": '<img src="./img/001-vegetable.png" alt="Sans viande">'
+    };
+
+    return classes
+        .filter(cls => relevantClasses.includes(cls))
+        .map(cls => `<div class="component">${classToIcon[cls]} ${cls}</div>`)
+        .join(' ');
+}
+
+// Chargez initialement tous les plats
+loadPlats();
